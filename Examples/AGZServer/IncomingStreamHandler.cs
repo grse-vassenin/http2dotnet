e1e0abe3ba65befb62dae407af3e10e3d26cedfa
Andrew Guz <andrew.guz@grse.de>
2023-02-09 20:45:43 +0300

    At leaset AGZServer can accept https connection.

---
+++ Examples/AGZServer/IncomingStreamHandler.cs
@@ -0,0 +1,13 @@
+﻿using Http2;
+using System.Threading.Tasks;
+
+namespace AGZServer
+{
+    internal static class IncomingStreamHandler
+    {
+        public static async Task HandleStream(IStream stream)
+        {
+            //do the work - read request and send the answer
+        }
+    }
+}
--- Examples/AGZServer/Server.cs
+++ Examples/AGZServer/Server.cs
@@ -1,5 +1,12 @@
-﻿using System.Net;
+﻿using Http2;
+using Http2.Hpack;
+using System.IO;
+using System.Net;
+using System.Net.Security;
 using System.Net.Sockets;
+using System.Reflection;
+using System.Security.Authentication;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 
 namespace AGZServer
@@ -10,17 +17,67 @@ namespace AGZServer
 
         public int Port { get; set; }
 
-        private TcpListener listener;
-
         public Server()
         {
-            listener = new TcpListener(Host, Port);
-            listener.Start();
         }
 
         public async Task Run()
         {
+            var listener = new TcpListener(Host, Port);
+            listener.Start();
 
+            var config = new ConnectionConfigurationBuilder(true)
+                    .UseStreamListener((stream) =>
+                    {
+                        IncomingStreamHandler.HandleStream(stream);
+                        return true;
+                    })
+                    .UseSettings(Settings.Default)
+                    .UseHuffmanStrategy(HuffmanStrategy.IfSmaller)
+                    .Build();
+
+            while (true)
+            {
+                var socket = await AcceptSocket(listener);
+                var sslStream = await Handshake(socket);
+                var wrappedStreams = sslStream.CreateStreams();
+                await HandleConnection(wrappedStreams.ReadableStream, wrappedStreams.WriteableStream);
+            }
+        }
+
+        private async Task<Socket> AcceptSocket(TcpListener listener)
+        {
+            var socket = await listener.AcceptSocketAsync();
+            socket.NoDelay = true;
+            return socket;
+        }
+
+        private static byte[] ReadWholeStream(Stream stream)
+        {
+            byte[] buffer = new byte[16 * 1024];
+            using (MemoryStream ms = new MemoryStream())
+            {
+                int read;
+                while ((read = stream.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    ms.Write(buffer, 0, read);
+                }
+
+                return ms.ToArray();
+            }
+        }
+
+        private async Task<SslStream> Handshake(Socket socket)
+        {
+            var sslStream = new SslStream(new NetworkStream(socket, true));
+            var serverCertificate = new X509Certificate2(ReadWholeStream(Assembly.GetExecutingAssembly().GetManifestResourceStream("AGZServer.localhost.p12")));
+            await sslStream.AuthenticateAsServerAsync(serverCertificate, false, SslProtocols.Tls12, false);
+            return sslStream;
+        }
+
+        private async Task HandleConnection(IReadableByteStream readableStream, IWriteAndCloseableByteStream writableStream)
+        {
+            //here we want to see that connection is done via http1.1 and first request is upgrade request and do the upgrade
         }
     }
 }
