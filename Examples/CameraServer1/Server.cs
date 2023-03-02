2cb4e311900252b3e79028e747179c983b9624a9
Andrew Guz <andrew.guz@grse.de>
2023-03-02 20:32:16 +0300

    Somehow working example of soap request
    Current problems
    - can't close connection
    - crash if after connection sending new setUplink to camera

--- Examples/CameraServer1/Server.cs
+++ Examples/CameraServer1/Server.cs
@@ -2,6 +2,7 @@
 using Http2.Hpack;
 using System;
 using System.IO;
+using System.Linq;
 using System.Net;
 using System.Net.Security;
 using System.Net.Sockets;
@@ -39,10 +40,60 @@ namespace CameraServer1
             }
         }
 
+        public async Task<string> SendSoapRequest(Connection connection, string host, int port, string path, string soapRequest)
+        {
+            Console.WriteLine("SendSoapRequest");
+            Console.WriteLine("Request:");
+            Console.WriteLine(soapRequest);
+            var soapRequestBytes = Encoding.UTF8.GetBytes(soapRequest);
+            //create headers
+            var hostValue = !string.IsNullOrEmpty(host) ? host : "0.0.0.0";
+            var headers = new HeaderField[]
+            {
+                new HeaderField { Name = ":method", Value = "POST" },
+                new HeaderField { Name = ":scheme", Value = "https" },
+                new HeaderField { Name = ":path", Value = path },
+                new HeaderField { Name = ":authority", Value = $"{hostValue}:{port}" },
+                new HeaderField { Name = "content-type", Value = "application/soap+xml; charset=utf-8" },
+                new HeaderField { Name = "content-length", Value = $"{soapRequestBytes.Length}" }
+            };
+            //crate a stream which will also write (send) all headers
+            var stream = await connection.CreateStreamAsync(headers);
+            //now send request content
+            await stream.WriteAsync(new ArraySegment<byte>(soapRequestBytes));
+            //read response headers
+            var responseHeaders = await stream.ReadHeadersAsync();
+            if (!responseHeaders.Any(h => h.Name == ":status") ||
+                responseHeaders.First(h => h.Name == ":status").Value != "200" ||
+                !responseHeaders.Any(h => h.Name == "content-length"))
+                return null;
+            var contentLength = int.Parse(responseHeaders
+                .First(h => h.Name == "content-length")
+                .Value);
+            //read response body
+            var fullBuffer = new byte[contentLength];
+            int offset = 0;
+            while (true)
+            {
+                var readResult = await stream.ReadAsync(new ArraySegment<byte>(fullBuffer, offset, contentLength - offset));
+                if (readResult.EndOfStream)
+                    break;
+                offset += readResult.BytesRead;
+                if (offset >= contentLength)
+                    break;
+            }
+            var soapReply = Encoding.UTF8.GetString(fullBuffer);
+            Console.WriteLine("Reply:");
+            Console.WriteLine(soapReply);
+            return soapReply;
+
+        }
+
         private async Task<Socket> AcceptSocket(TcpListener listener)
         {
             var socket = await listener.AcceptSocketAsync();
             socket.NoDelay = true;
+            Console.WriteLine("Socket opened");
             return socket;
         }
 
@@ -127,7 +178,17 @@ namespace CameraServer1
                 .Build();
             var connection = new Connection(connectionConfiguration, wrappedStreams.ReadableStream, wrappedStreams.WriteableStream);
 
+            var soapRequest =
+                "<?xml version=\"1.0\" encoding=\"utf-8\"?>\r\n" +
+                "<s:Envelope xmlns:s=\"http://www.w3.org/2003/05/soap-envelope\" xmlns:tds=\"http://www.onvif.org/ver10/device/wsdl\" xmlns:tt=\"http://www.onvif.org/ver10/schema\">\r\n" +
+                "<s:Body>\r\n" +
+                "<tds:GetServices>\r\n" +
+                "<tds:IncludeCapability>true</tds:IncludeCapability>\r\n" +
+                "</tds:GetServices>\r\n" +
+                "</s:Body>\r\n" +
+                "</s:Envelope>";
 
+            await SendSoapRequest(connection, "", Port, "/onvif/uplink_service", soapRequest);
 
             //await connection.GoAwayAsync(ErrorCode.NoError, true);
         }
