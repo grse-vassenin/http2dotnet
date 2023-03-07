b61399b46454f97a2ca47cb74730d210d6fc5afc
Andrew Guz <andrew.guz@grse.de>
2023-03-07 10:35:04 +0300

    Still problems with soap request but more or less working.

--- Examples/TestApp/Form1.cs
+++ Examples/TestApp/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Forms;
+﻿using System.Threading.Tasks;
+using System.Windows.Forms;
 using UplinkLib;
 
 namespace TestApp
@@ -23,12 +24,12 @@ namespace TestApp
 
         private void OnProcessConnectionEvent(ConnectionWrapper connectionWrapper)
         {
-            /*if (client != null)
+            if (client != null)
+            //why meany connections?
             {
-                //why meany connections?
-                var closeTask = client.Close();
-                closeTask.Wait();
-            }*/
+                Task closeTaks;
+                closeTaks = client.Close();
+            }
             client = new UplinkClient()
             {
                 ConnectionWrapper = connectionWrapper
@@ -50,7 +51,8 @@ namespace TestApp
             if (client != null)
             {
                 sendRequest_btn.Enabled = false;
-                client.Close().Wait();
+                Task closeTaks;
+                closeTaks = client.Close();
                 client = null;
             }
             server.Stop();
@@ -58,13 +60,27 @@ namespace TestApp
 
         private void sendRequest_btn_Click(object sender, System.EventArgs e)
         {
+            var soapRequest =
+                "<?xml version=\"1.0\" encoding=\"utf-8\"?>\r\n" +
+                "<s:Envelope xmlns:s=\"http://www.w3.org/2003/05/soap-envelope\" xmlns:tds=\"http://www.onvif.org/ver10/device/wsdl\" xmlns:tt=\"http://www.onvif.org/ver10/schema\">\r\n" +
+                "<s:Body>\r\n" +
+                "<tds:GetServices>\r\n" +
+                "<tds:IncludeCapability>true</tds:IncludeCapability>\r\n" +
+                "</tds:GetServices>\r\n" +
+                "</s:Body>\r\n" +
+                "</s:Envelope>";
             replyBody_txt.Clear();
             if (client == null ||
                 requestPath_txt.Text.Length == 0 ||
                 requestBody_txt.Text.Length == 0)
                 return;
-            var reply = client.SendSoapRequest(requestPath_txt.Text, requestBody_txt.Text).Result;
-            replyBody_txt.Text = reply;
+            //var replyTask = client.SendSoapRequest(requestPath_txt.Text, requestBody_txt.Text);
+            var replyTask = client.SendSoapRequest(requestPath_txt.Text, soapRequest);
+            var replyAwaiter = replyTask.GetAwaiter();
+            replyAwaiter.OnCompleted(() =>
+            {
+                replyBody_txt.Text = replyAwaiter.GetResult();
+            });
         }
     }
 }
