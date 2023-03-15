5eb7404849c4e73660f731cfed5d3bc069666cd5
Andrew Guz <andrew.guz@grse.de>
2023-03-15 20:45:10 +0300

    Two applications.

---
+++ Examples/CameraApp/CameraForm.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace CameraApp
+{
+    partial class CameraForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.connect_btn = new System.Windows.Forms.Button();
+            this.log_txt = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // connect_btn
+            // 
+            this.connect_btn.Location = new System.Drawing.Point(12, 12);
+            this.connect_btn.Name = "connect_btn";
+            this.connect_btn.Size = new System.Drawing.Size(75, 23);
+            this.connect_btn.TabIndex = 1;
+            this.connect_btn.Text = "Connect";
+            this.connect_btn.UseVisualStyleBackColor = true;
+            this.connect_btn.Click += new System.EventHandler(this.connect_btn_Click);
+            // 
+            // log_txt
+            // 
+            this.log_txt.Location = new System.Drawing.Point(12, 41);
+            this.log_txt.Multiline = true;
+            this.log_txt.Name = "log_txt";
+            this.log_txt.Size = new System.Drawing.Size(419, 397);
+            this.log_txt.TabIndex = 0;
+            // 
+            // CameraForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.connect_btn);
+            this.Controls.Add(this.log_txt);
+            this.Name = "CameraForm";
+            this.Text = "UplinkForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button connect_btn;
+        private System.Windows.Forms.TextBox log_txt;
+    }
+}
---
+++ Examples/CameraApp/CameraForm.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Sockets;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Http2;
+using Http2.Hpack;
+
+namespace CameraApp
+{
+    public partial class CameraForm : Form
+    {
+        private Socket socket;
+        private Connection connection;
+
+        public CameraForm()
+        {
+            InitializeComponent();
+
+        }
+
+        static async Task HandleIncomingStream(IStream stream)
+        {
+
+        }
+
+        static bool AcceptIncomingStream(IStream stream)
+        {
+            var task = HandleIncomingStream(stream);
+            return true;
+        }
+
+        private async void connect_btn_Click(object sender, EventArgs e)
+        {
+            var tcpClient = new TcpClient();
+            await tcpClient.ConnectAsync("127.0.0.1", 8888);
+            log_txt.AppendText("Connected\r\n");
+            socket = tcpClient.Client;
+            socket.NoDelay = true;
+
+            var wrappedStreams = socket.CreateStreams();
+            var config = new ConnectionConfigurationBuilder(true)
+                    .UseStreamListener(AcceptIncomingStream)
+                    .UseSettings(Settings.Default)
+                    .UseHuffmanStrategy(HuffmanStrategy.IfSmaller)
+                    .Build();
+            connection = new Connection(config, wrappedStreams.ReadableStream, wrappedStreams.WriteableStream);
+            var remoteGoAwayTask = connection.RemoteGoAwayReason;
+            var closeWhenRemoteGoAway = Task.Run(async () =>
+            {
+                await remoteGoAwayTask;
+                await connection.GoAwayAsync(ErrorCode.NoError, true);
+            });
+        }
+    }
+}
